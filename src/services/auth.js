9afe37b50634ac55bf8359ba45cec8ea3b2908cf
Zachariah Nagatani <zachnagatani@gmail.com>
2017-01-12 18:50:21 -0800

    Feat: Auth Service

---
+++ src/services/auth.js
@@ -0,0 +1,42 @@
+const auth = {
+    saveToken: (token) => {
+        $window.localStorage['mean-token'] = token;
+    },
+
+    getToken: () => {
+        return $window.localStorage['mean-token'];
+    },
+
+    logout: () => {
+        $window.localStorage.removeItem('mean-token');
+    },
+
+    isLoggedIn: function() {
+        const token = this.getToken();
+        let payload;
+
+        if(token) {
+            payload = token.split('.')[1];
+            payload = $window.atob(payload);
+            payload = JSON.parse(payload);
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    },
+
+    currentUser: function() {
+        if(self.isLoggedIn()) {
+            const token = this.getToken();
+            let payload = token.split('.')[1];
+            payload = window.atob(payload);
+            payload = JSON.parse(payload);
+
+            return {
+                username: payload.username,
+            };
+        }
+    }
+};
+
+export default auth;
